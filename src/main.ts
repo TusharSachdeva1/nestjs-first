f6bca92f3f797d0eed3544826db4d5ed213b3b99
Tushar <tushar.sachdeva@datachannel.co>
2023-05-11 17:45:11 +0530

    fifth commit-- added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Title')
+    .setDescription('Desc')
+    .setVersion('version')
+    .addTag('tag')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+  
   await app.listen(3000, '0.0.0.0');
   console.log("server is live");
 }
